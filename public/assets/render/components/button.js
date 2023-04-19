9aebc5272021120a8f57bd9a563c7c9ab8b7a8d7
Mikkel RINGAUD <mikkel@milescode.dev>
2023-04-19 16:02:04 +0200

    feat(button): on peut passer un élément en tant que contenu de bouton

--- public/assets/render/components/button.js
+++ public/assets/render/components/button.js
@@ -4,20 +4,20 @@ import { classNames, createElement } from "../../utils/helpers.js";
 
 /**
  * @param {Object} props
- * @param {string} props.content
+ * @param {string | HTMLElementTagNameMap[keyof HTMLElementTagNameMap]} props.children
  * @param {"text" | "primary"} [props.color]
  * @param {"button" | "submit"} [props.type]
  * @returns {HTMLButtonElement}
  */
 export const createButtonComponent = (props = {
-  content: "",
+  children: "",
   color: "text",
   type: "button"
 }) => createElement("button", {
   type: props.type ?? "button",
   class: classNames(
-    "border hover:font-medium px-4 py-2 hover:scale-105 transition-[transform,background]",
+    "group w-full border hover:font-medium px-4 py-2 hover:scale-105 transition-[transform,background]",
     props.color === "text" && "border-[#4C4F69] text-[#4C4F69] hover:bg-[#4C4F69] hover:text-[#EFF1F5]",
     props.color === "primary" && "border-[#7287fd] text-[#7287fd] hover:bg-[#7287fd] hover:text-[#EFF1F5]"
   )
-}, props.content ?? "")
+}, props.children)
