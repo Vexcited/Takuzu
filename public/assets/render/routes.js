a1a57175e11e263d235f7b697a2703d35f765f55
Mikkel RINGAUD <mikkel@milescode.dev>
2023-04-19 16:01:08 +0200

    feat: ajout du `state` dans navigate et on enlève les `/` à la fin des routes

--- public/assets/render/routes.js
+++ public/assets/render/routes.js
@@ -1,4 +1,6 @@
 import RenderPageHome from "./pages/index.js";
+import RenderPageSolo from "./pages/solo/index.js";
+import RenderPageSoloGame from "./pages/solo/game.js";
 import RenderPageOnline from "./pages/online.js";
 
 /** @type {InstanceType<router[keyof router]>} */
@@ -6,7 +8,9 @@ export let current = null;
 
 /** Permet d'initialiser le router. */
 export const initialize = () => {
-  const route = window.location.pathname.trim();
+  let route = window.location.pathname.trim();
+  route = route.endsWith('/') ? route.substring(0, route.length - 1) : route;
+
   current = router[route] ? new router[route]() : null;
 
   // On redirige sur la page d'accueil pour les erreurs 404.
@@ -16,15 +20,18 @@ export const initialize = () => {
 /**
  * Permet de naviguer vers une nouvelle route.
  * @param {keyof typeof router} route
+ * @param {Record<string, string | number>} [state]
  */
-export const navigate = (route) => {
-  current.destroy();
+export const navigate = (route, state = {}) => {
+  if (current) current.destroy();
 
-  window.history.pushState({}, "", route);
+  window.history.pushState(state, "", route);
   current = new router[route]();
 }
 
 const router = /** @type {const} */ ({
   "/": RenderPageHome,
+  "/solo": RenderPageSolo,
+  "/solo/game": RenderPageSoloGame,
   "/online": RenderPageOnline
 });
