0839fd4d76072658af7a92b2987b1951186ea13a
Mikkel RINGAUD <mikkel@milescode.dev>
2023-06-08 19:36:47 +0200

    chore: self host fonts/tailwindcss

---
+++ sources/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import("tailwindcss").Config} */
+export default {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Jost", "sans-serif"]
+    }
+  },
+  plugins: []
+}
